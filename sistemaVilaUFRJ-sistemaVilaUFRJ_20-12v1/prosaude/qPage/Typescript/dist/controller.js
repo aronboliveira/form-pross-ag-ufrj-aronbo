9e5bdae495ff2ef7be6011f144ecb4334bea9756
Aron-PC <aron.b.96@gmail.com>
2023-12-26 15:40:31 -0300

    Código reestruturado para desenvolvimento direto em TS + Instalação inicial do Vite para bundling futuro + Instalação inicial do Babel para transpilação futura (p/ browser legado) + Instalação do React para renderização dinâmica (testando se vale a pena ainda)

--- sistemaVilaUFRJ-sistemaVilaUFRJ_20-12v1/prosaude/qPage/Typescript/dist/controller.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//nesse file ocorrem principalmente as adições de listeners, sincronização das chamadas de funções para manipulação de informação/layout e validação dos elementos no DOM
